21fe18cd9dd8bd7a3c7f8f613ad2c1c740b88cc0
weikeng <w.k@berkeley.edu>
2019-06-18 16:03:00 -0700

    show more error messages

--- src/ext/oblivc/obliv_bits.c
+++ src/ext/oblivc/obliv_bits.c
@@ -945,17 +945,18 @@ static ProtocolTransport* tls2PSplit(ProtocolTransport* tsrc){
     SSL_set_accept_state(ssl);
   }
 
-  if(SSL_do_handshake(ssl) != 1){
+  int handshake_error_code  = SSL_do_handshake(ssl);
+  if(handshake_error_code != 1){
     TLS_LOG_ERROR("Handshake failed for split connections");
 
-    printf("The error number returned by SSL is: %d\n", SSL_get_error(ssl, error));
+    printf("The error number returned by SSL is: %d\n", SSL_get_error(ssl, handshake_error_code));
 
     char error_string[256];
     int err_in_queue;
     while(err_in_queue = ERR_get_error()){
       printf("An error in the queue: %s\n", ERR_error_string(err_in_queue, error_string));
     }
-    
+
     return NULL;
   }
 
